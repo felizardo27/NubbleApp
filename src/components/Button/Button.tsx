5f8fe995670b4e5a52efea17a504bba1a9fd5124
João Pedro Felizardo <jp.felizardo27@gmail.com>
2023-12-14 16:15:41 -0300

    box component

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,6 +3,7 @@ import {TouchableOpacity} from 'react-native';
 import {Text} from '../Text/Text';
 import {useTheme} from '@shopify/restyle';
 import {Theme} from '../../theme/theme';
+import {Box} from '../Box/Box';
 
 interface ButtonProps {
   title: string;
@@ -11,17 +12,20 @@ interface ButtonProps {
 export function Button({title}: ButtonProps) {
   const {colors} = useTheme<Theme>();
   return (
-    <TouchableOpacity
-      style={{
-        paddingHorizontal: 20,
-        paddingVertical: 14,
-        backgroundColor: colors.carrotSecondary,
-        alignItems: 'center',
-        borderRadius: 16,
-      }}>
+    <Box
+      backgroundColor="greenPrimary"
+      paddingHorizontal="s20"
+      // style={{
+      //   paddingHorizontal: 20,
+      //   paddingVertical: 14,
+      //   backgroundColor: colors.carrotSecondary,
+      //   alignItems: 'center',
+      //   borderRadius: 16,
+      // }}
+    >
       <Text preset="paragraphMedium" bold style={{color: '#fff'}}>
         {title}
       </Text>
-    </TouchableOpacity>
+    </Box>
   );
 }
