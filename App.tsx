68100927950b606b2a10c1ff58c63203480c2850
João Pedro Felizardo <jp.felizardo27@gmail.com>
2024-02-25 12:51:09 -0300

    Design System: Restyle Theme

--- App.tsx
+++ App.tsx
@@ -1,24 +1,21 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
 
 import {SafeAreaView, View} from 'react-native';
 import {Text} from './src/components/Text/Text';
 import {Button} from './src/components/Button/Button';
+import {ThemeProvider} from '@shopify/restyle';
+import {theme} from './src/theme/theme';
 
 function App() {
   return (
-    <SafeAreaView>
-      <View style={{paddingHorizontal: 24}}>
-        <Text preset="headingLarge">CoffStack</Text>
-        <Button title="Entrar" />
-      </View>
-    </SafeAreaView>
+    <ThemeProvider theme={theme}>
+      <SafeAreaView>
+        <View style={{paddingHorizontal: 24}}>
+          <Text preset="headingLarge">CoffStack</Text>
+          <Button title="Entrar" />
+        </View>
+      </SafeAreaView>
+    </ThemeProvider>
   );
 }
 
---
+++ src/theme/theme.ts
@@ -0,0 +1,43 @@
+import {createTheme} from '@shopify/restyle';
+
+const pallete = {
+  greenPrimary: '#074C4E',
+  greenPrimaryLight: '#EAF6F6',
+  carrotSecondary: '#F86F2D',
+  carrotSecondaryLight: '#FAE6DD',
+  greenSuccess: '#4ABC86',
+  greenSuccessLight: '#D8FFEC',
+  redError: '#EA3838',
+  redErrorLight: '#EA3838',
+
+  grayBlack: '#000000',
+  gray1: '#636363',
+  gra2: '#8E8E8E',
+  gray3: '#B3B3B3',
+  gray4: '#E1E1E1',
+  gray5: '#F5F5F5',
+  grayWhite: '#FFFFFF',
+};
+
+export const theme = createTheme({
+  colors: {
+    ...pallete,
+    primary: pallete.greenPrimary,
+    primaryContrast: pallete.grayWhite,
+
+    buttonPrimary: pallete.greenPrimary,
+
+    background: pallete.grayWhite,
+    backgroundContrast: pallete.grayBlack,
+
+    error: pallete.redError,
+    errorLight: pallete.redErrorLight,
+
+    success: pallete.greenSuccess,
+    successLight: pallete.greenSuccessLight,
+  },
+  spacing: {},
+  borderRadii: {},
+});
+
+export type Theme = typeof theme;
