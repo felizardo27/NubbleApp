df30c8ca743a9f532645f72b8ac8bcfd52d0bb8e
João Pedro Felizardo <jp.felizardo27@gmail.com>
2024-03-19 05:35:20 -0300

    Design System: TouchableOpacityBox

--- src/components/Box/Box.tsx
+++ src/components/Box/Box.tsx
@@ -1,4 +1,33 @@
-import {createBox} from '@shopify/restyle';
+import {
+  createBox,
+  createRestyleComponent,
+  backgroundColor,
+  BackgroundColorProps,
+  spacing,
+  SpacingProps,
+  spacingShorthand,
+  SpacingShorthandProps,
+  layout,
+  LayoutProps,
+  border,
+  BorderProps,
+} from '@shopify/restyle';
 import {Theme} from '../../theme/theme';
+import {TouchableOpacity, TouchableOpacityProps} from 'react-native';
 
 export const Box = createBox<Theme>();
+
+export type TouchableOpacityboxProps = BackgroundColorProps<Theme> &
+  SpacingProps<Theme> &
+  SpacingShorthandProps<Theme> &
+  LayoutProps<Theme> &
+  BorderProps<Theme> &
+  TouchableOpacityProps;
+
+export const TouchableOpacityBox = createRestyleComponent<
+  TouchableOpacityboxProps,
+  Theme
+>(
+  [backgroundColor, spacing, spacingShorthand, layout, border],
+  TouchableOpacity,
+);
