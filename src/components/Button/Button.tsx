fa9c789eb35f03448c1b38724aafe13618b12041
João Pedro Felizardo <jp.felizardo27@gmail.com>
2024-03-18 23:20:31 -0300

    Design System: Button - Propriedade borderRadius

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,8 +1,5 @@
 import React from 'react';
-// import {TouchableOpacity} from 'react-native';
 import {Text} from '../Text/Text';
-// import {useTheme} from '@shopify/restyle';
-// import {Theme} from '../../theme/theme';
 import {Box} from '../Box/Box';
 import {ActivityIndicator} from 'react-native';
 
@@ -12,7 +9,6 @@ interface ButtonProps {
 }
 
 export function Button({title, loading}: ButtonProps) {
-  // const {colors} = useTheme<Theme>();
   return (
     <Box
       backgroundColor="buttonPrimary"
@@ -20,14 +16,7 @@ export function Button({title, loading}: ButtonProps) {
       height={50}
       alignItems="center"
       justifyContent="center"
-      // style={{
-      //   paddingHorizontal: 20,
-      //   paddingVertical: 14,
-      //   backgroundColor: colors.carrotSecondary,
-      //   alignItems: 'center',
-      //   borderRadius: 16,
-      // }}
-    >
+      borderRadius="s16">
       {loading ? (
         <ActivityIndicator />
       ) : (
