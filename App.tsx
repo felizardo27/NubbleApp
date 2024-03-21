bbffd0da535ebe04e4161afedb77d2dd7def78b6
João Pedro Felizardo <jp.felizardo27@gmail.com>
2024-03-21 11:14:33 -0300

    Icon Component: Biblioteca de Icons

--- App.tsx
+++ App.tsx
@@ -18,9 +18,37 @@ function App() {
             flexDirection="row"
             alignItems="center"
             justifyContent="center"
+            flexWrap="wrap"
             gap="s20">
-            <Icon name="eyeOn" color="error" size={100} />
-            <Icon name="eyeOff" color="carrotSecondary" size={70} />
+            <Icon name="arrowLeft" size={60} />
+            <Icon name="arrowRight" size={60} />
+            <Icon name="bell" size={60} />
+            <Icon name="bellOn" size={60} />
+            <Icon name="bookmark" size={60} />
+            <Icon name="bookmarkFill" size={60} />
+            <Icon name="camera" size={60} />
+            <Icon name="chat" size={60} />
+            <Icon name="chatOn" size={60} />
+            <Icon name="check" size={60} />
+            <Icon name="chevronRight" size={60} />
+            <Icon name="comment" size={60} />
+            <Icon name="eyeOff" size={60} />
+            <Icon name="eyeOn" size={60} />
+            <Icon name="flashOff" size={60} />
+            <Icon name="flashOn" size={60} />
+            <Icon name="heart" size={60} />
+            <Icon name="heartFill" size={60} />
+            <Icon name="home" size={60} />
+            <Icon name="homeFill" size={60} />
+            <Icon name="message" size={60} />
+            <Icon name="more" size={60} />
+            <Icon name="newPost" size={60} />
+            <Icon name="profile" size={60} />
+            <Icon name="profileFill" size={60} />
+            <Icon name="search" size={60} />
+            <Icon name="send" size={60} />
+            <Icon name="settings" size={60} />
+            <Icon name="trash" size={60} />
           </Box>
         </View>
       </SafeAreaView>
--- src/components/Icon/Icon.tsx
+++ src/components/Icon/Icon.tsx
@@ -1,8 +1,36 @@
 import React from 'react';
-import {EyeOnIcon} from '../../assets/icons/EyeOnIcon';
-import {EyeOffIcon} from '../../assets/icons/EyeOffIcon';
 import {useAppTheme} from '../../hooks/useAppTheme';
 import {ThemeColors} from '../../theme/theme';
+
+import {EyeOffIcon} from '../../assets/icons/EyeOffIcon';
+import {EyeOnIcon} from '../../assets/icons/EyeOnIcon';
+import {ArrowLeftIcon} from '../../assets/icons/ArrowLeftIcon';
+import {ArrowRightIcon} from '../../assets/icons/ArrowRightIcon';
+import {BellIcon} from '../../assets/icons/BellIcon';
+import {BellOnIcon} from '../../assets/icons/BellOnIcon';
+import {BookmarkIcon} from '../../assets/icons/BookmarkIcon';
+import {CameraIcon} from '../../assets/icons/CameraIcon';
+import {ChatIcon} from '../../assets/icons/ChatIcon';
+import {ChatOnIcon} from '../../assets/icons/ChatOnIcon';
+import {CheckIcon} from '../../assets/icons/CheckIcon';
+import {ChevronRightIcon} from '../../assets/icons/ChevronRightIcon';
+import {CommentIcon} from '../../assets/icons/CommentIcon';
+import {FlashOffIcon} from '../../assets/icons/FlashOffIcon';
+import {FlashOnIcon} from '../../assets/icons/FlashOnIcon';
+import {HeartIcon} from '../../assets/icons/HeartIcon';
+import {HeartFillIcon} from '../../assets/icons/HeartFillIcon';
+import {HomeIcon} from '../../assets/icons/HomeIcon';
+import {HomeFillIcon} from '../../assets/icons/HomeFillIcon';
+import {MessageIcon} from '../../assets/icons/MessageIcon';
+import {MoreIcon} from '../../assets/icons/MoreIcon';
+import {NewPostIcon} from '../../assets/icons/NewPostIcon';
+import {ProfileIcon} from '../../assets/icons/ProfileIcon';
+import {ProfileFillIcon} from '../../assets/icons/ProfileFillIcon';
+import {SearchIcon} from '../../assets/icons/SearchIcon';
+import {SendIcon} from '../../assets/icons/SendIcon';
+import {SettingsIcon} from '../../assets/icons/SettingsIcon';
+import {TrashIcon} from '../../assets/icons/TrashIcon';
+import {BookmarkFillIcon} from '../../assets/icons/BookmarkFillIcon';
 
 export interface IconBase {
   size?: number;
@@ -22,8 +50,35 @@ export function Icon({name, color = 'backgroundContrast', size}: Props) {
 }
 
 const iconRegistry = {
-  eyeOn: EyeOnIcon,
+  arrowLeft: ArrowLeftIcon,
+  arrowRight: ArrowRightIcon,
+  bell: BellIcon,
+  bellOn: BellOnIcon,
+  bookmark: BookmarkIcon,
+  bookmarkFill: BookmarkFillIcon,
+  camera: CameraIcon,
+  chat: ChatIcon,
+  chatOn: ChatOnIcon,
+  check: CheckIcon,
+  chevronRight: ChevronRightIcon,
+  comment: CommentIcon,
   eyeOff: EyeOffIcon,
+  eyeOn: EyeOnIcon,
+  flashOff: FlashOffIcon,
+  flashOn: FlashOnIcon,
+  heart: HeartIcon,
+  heartFill: HeartFillIcon,
+  home: HomeIcon,
+  homeFill: HomeFillIcon,
+  message: MessageIcon,
+  more: MoreIcon,
+  newPost: NewPostIcon,
+  profile: ProfileIcon,
+  profileFill: ProfileFillIcon,
+  search: SearchIcon,
+  send: SendIcon,
+  settings: SettingsIcon,
+  trash: TrashIcon,
 };
 
 type IconType = typeof iconRegistry;
