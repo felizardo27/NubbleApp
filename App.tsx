15c9cc3c0df2432a5b4d17d312a9a42a85ffdef5
João Pedro Felizardo <jp.felizardo27@gmail.com>
2024-03-24 10:47:52 -0300

    Screen Component: Lindando com Safe Area

--- App.tsx
+++ App.tsx
@@ -3,12 +3,15 @@ import {ThemeProvider} from '@shopify/restyle';
 import {theme} from './src/theme/theme';
 
 import {LoginScreen} from './src/screens/auth/LoginScreen/LoginScreen';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <LoginScreen />
-    </ThemeProvider>
+    <SafeAreaProvider>
+      <ThemeProvider theme={theme}>
+        <LoginScreen />
+      </ThemeProvider>
+    </SafeAreaProvider>
   );
 }
 
--- src/components/Screen/Screen.tsx
+++ src/components/Screen/Screen.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import {Box} from '../Box/Box';
+import {useAppSafeArea} from '../../hooks/useAppSafeArea';
 
 interface ScreenProps {
   children: React.ReactNode;
 }
 
 export function Screen({children}: ScreenProps) {
-  return <Box padding="s24">{children}</Box>;
+  const {top} = useAppSafeArea();
+
+  return (
+    <Box padding="s24" style={{paddingTop: top}}>
+      {children}
+    </Box>
+  );
 }
