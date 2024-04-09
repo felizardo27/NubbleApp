7a406d998b8a203bf50932c8eab2c800c40c8f6d
João Pedro Felizardo <jp.felizardo27@gmail.com>
2024-04-09 07:00:05 -0300

    Advanced navigation: Nested Navigator

--- src/screens/app/HomeScreen/HomeScreen.tsx
+++ src/screens/app/HomeScreen/HomeScreen.tsx
@@ -1,16 +1,15 @@
 import React from 'react';
 
 import {Button, Screen, Text} from '@components';
-// import {AppScreenProps} from '@routes';
+import {AppTabScreenProps} from '@routes';
 
-// export function HomeScreen({navigation}: AppScreenProps<'HomeScreen'>) {
-export function HomeScreen() {
+export function HomeScreen({navigation}: AppTabScreenProps<'HomeScreen'>) {
   return (
     <Screen>
       <Text preset="headingLarge">Home Screen</Text>
       <Button
         title="Settings"
-        // onPress={() => navigation.navigate('SettingsScreen')}
+        onPress={() => navigation.navigate('SettingsScreen')}
       />
     </Screen>
   );
--- src/screens/app/SettingsScreen/SettingsScreen.tsx
+++ src/screens/app/SettingsScreen/SettingsScreen.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 
-import {Screen, Text} from '@components';
+import {Button, Screen, Text} from '@components';
 import {AppScreenProps} from '@routes';
 
-// type ScreenProps = NativeStackScreenProps<AppStackParamList, 'SettingsScreen'>;
-
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export function SettingsScreen({navigation}: AppScreenProps<'SettingsScreen'>) {
   return (
     <Screen canGoBack>
       <Text preset="headingSmall">Settings Screen</Text>
+      <Button
+        title="New Post"
+        onPress={() =>
+          navigation.navigate('AppTabNavigator', {
+            screen: 'NewPostScreen',
+          })
+        }
+      />
+      <Button
+        title="FavoriteScreen"
+        mt="s10"
+        onPress={() =>
+          navigation.navigate('AppTabNavigator', {
+            screen: 'FavoriteScreen',
+          })
+        }
+      />
     </Screen>
   );
 }
