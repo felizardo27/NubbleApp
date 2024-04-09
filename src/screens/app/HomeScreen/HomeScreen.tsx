84eb57ac69a4e6e68c139db2f99bb2fcbe355164
João Pedro Felizardo <jp.felizardo27@gmail.com>
2024-04-08 23:42:22 -0300

    Advanced navigation: Bottom Tab Navigator and Nesting Navigator

--- src/screens/app/HomeScreen/HomeScreen.tsx
+++ src/screens/app/HomeScreen/HomeScreen.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 
 import {Button, Screen, Text} from '@components';
-import {AppScreenProps} from '@routes';
+// import {AppScreenProps} from '@routes';
 
-export function HomeScreen({navigation}: AppScreenProps<'HomeScreen'>) {
+// export function HomeScreen({navigation}: AppScreenProps<'HomeScreen'>) {
+export function HomeScreen() {
   return (
     <Screen>
       <Text preset="headingLarge">Home Screen</Text>
       <Button
         title="Settings"
-        onPress={() => navigation.navigate('SettingsScreen')}
+        // onPress={() => navigation.navigate('SettingsScreen')}
       />
     </Screen>
   );
