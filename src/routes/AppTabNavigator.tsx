5394d353c8a610a80757fca38ad32041907b5154
João Pedro Felizardo <jp.felizardo27@gmail.com>
2024-04-10 07:22:33 -0300

    Advanced navigation: Bottom Tab Customization

--- src/routes/AppTabNavigator.tsx
+++ src/routes/AppTabNavigator.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
+import {Icon, Text} from '@components';
 import {
   FavoriteScreen,
   HomeScreen,
@@ -23,8 +24,30 @@ export function AppTabNavigator() {
     <Tab.Navigator
       screenOptions={{
         headerShown: false,
+        tabBarStyle: {
+          paddingTop: 10,
+        },
       }}>
-      <Tab.Screen name="HomeScreen" component={HomeScreen} />
+      <Tab.Screen
+        options={{
+          tabBarLabel: ({focused}) => (
+            <Text
+              semiBold
+              preset="paragraphCaption"
+              color={focused ? 'primary' : 'backgroundContrast'}>
+              Início
+            </Text>
+          ),
+          tabBarIcon: ({focused}) => (
+            <Icon
+              name={focused ? 'homeFill' : 'home'}
+              color={focused ? 'primary' : 'backgroundContrast'}
+            />
+          ),
+        }}
+        name="HomeScreen"
+        component={HomeScreen}
+      />
       <Tab.Screen name="NewPostScreen" component={NewPostScreen} />
       <Tab.Screen name="FavoriteScreen" component={FavoriteScreen} />
       <Tab.Screen name="MyProfileScreen" component={MyProfileScreen} />
