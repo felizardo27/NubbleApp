e5ddc792fca0505d600600dfcc37adb8c8bdeb77
João Pedro Felizardo <jp.felizardo27@gmail.com>
2024-04-15 22:10:25 -0300

    Complex components: FlatList and renderItem

--- src/screens/app/HomeScreen/HomeScreen.tsx
+++ src/screens/app/HomeScreen/HomeScreen.tsx
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from 'react';
+import {Dimensions, FlatList, Image, ListRenderItemInfo} from 'react-native';
 
-import {Screen, Text} from '@components';
+import {Box, Screen, Text} from '@components';
 import {Post, postService} from '@domain';
 import {AppTabScreenProps} from '@routes';
 
@@ -12,11 +13,35 @@ export function HomeScreen({navigation}: AppTabScreenProps<'HomeScreen'>) {
     postService.getList().then(list => setPostList(list));
   }, []);
 
+  function renderItem({item}: ListRenderItemInfo<Post>) {
+    return (
+      <Box marginBottom="s24">
+        <Box flexDirection="row">
+          <Image
+            source={{uri: item.author.profileURL}}
+            width={32}
+            height={32}
+            borderRadius={50}
+          />
+          <Text>{item.author.userName}</Text>
+        </Box>
+        <Image
+          source={{uri: item.imageURL}}
+          resizeMode="cover"
+          width={Dimensions.get('screen').width}
+          height={300}
+        />
+      </Box>
+    );
+  }
+
   return (
     <Screen>
-      {postList.map(post => (
-        <Text>{post.text}</Text>
-      ))}
+      <FlatList
+        data={postList}
+        keyExtractor={item => item.id}
+        renderItem={renderItem}
+      />
     </Screen>
   );
 }
