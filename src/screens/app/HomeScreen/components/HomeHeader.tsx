5d1c067080a6ab45dad3b636e45b11175d512dc8
João Pedro Felizardo <jp.felizardo27@gmail.com>
2024-04-18 21:10:52 -0300

    Complex components: HomeHeader component LogoSVG

--- src/screens/app/HomeScreen/components/HomeHeader.tsx
+++ src/screens/app/HomeScreen/components/HomeHeader.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import {SimpleLogo} from '@brand';
+
 import {Box, BoxProps, Icon} from '@components';
 import {useAppSafeArea} from '@hooks';
 
@@ -7,7 +9,8 @@ export function HomeHeader() {
   const {top} = useAppSafeArea();
   return (
     <Box {...$wrapper} style={{paddingTop: top}}>
-      <Box backgroundColor="carrotSecondary" width={70} height={16} />
+      {/* <Box backgroundColor="carrotSecondary" width={70} height={16} /> */}
+      <SimpleLogo width={70} />
       <Box flexDirection="row" gap="s24">
         <Icon name="search" />
         <Icon name="bellOn" />
