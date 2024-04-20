86bef09b190b3387f2829a43c647a7799d43038e
João Pedro Felizardo <jp.felizardo27@gmail.com>
2024-04-20 11:21:57 -0300

    API integration: HTTP Client with  Axios

--- src/domain/Post/PostApi.ts
+++ src/domain/Post/PostApi.ts
@@ -1,21 +1,10 @@
-import {PageAPI} from '@api';
+import {api, PageAPI} from '@api';
 
 import {PostAPI} from './PostTypes';
 
 async function getList(): Promise<PageAPI<PostAPI>> {
-  //TODO: Simular delay api
-
-  let response = await fetch('http://localhost:3333/user/post', {
-    method: 'GET',
-    headers: {
-      Authorization:
-        'Bearer Mg.hE-Xl_WINEGqSr63OdEa_sf4i9NsGAtawQMIbmyV1jHoFWEFG8bHHnE-yDox',
-    },
-  });
-
-  let data: PageAPI<PostAPI> = await response.json();
-
-  return data;
+  const response = await api.get<PageAPI<PostAPI>>('user/post');
+  return response.data;
 }
 
 export const postApi = {
