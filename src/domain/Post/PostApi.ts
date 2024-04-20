633a78d42e40446830a815ccaa45fe335eec73c6
João Pedro Felizardo <jp.felizardo27@gmail.com>
2024-04-19 22:18:44 -0300

    API integration: fetch API

--- src/domain/Post/PostApi.ts
+++ src/domain/Post/PostApi.ts
@@ -3,7 +3,19 @@ import {Post} from './Types';
 
 async function getList(): Promise<Post[]> {
   //TODO: Simular delay api
-  await new Promise(resolve => setTimeout(() => resolve(''), 1000));
+
+  let response = await fetch('http://localhost:3333/user/post', {
+    method: 'GET',
+    headers: {
+      Authorization:
+        'Bearer Mg.hE-Xl_WINEGqSr63OdEa_sf4i9NsGAtawQMIbmyV1jHoFWEFG8bHHnE-yDox',
+    },
+  });
+
+  let data = await response.json();
+  console.log(data);
+
+  //await new Promise(resolve => setTimeout(() => resolve(''), 1000));
   return postListMock;
 }
 
