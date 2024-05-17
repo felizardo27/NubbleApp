a7126c3e184598208b9f7d27355d7083193cf778
João Pedro Felizardo <jp.felizardo27@gmail.com>
2024-05-16 23:09:43 -0300

    TanStack Query: adding isFetching and refetch to refresh screen

--- src/domain/User/useCases/useUserGetById.ts
+++ src/domain/User/useCases/useUserGetById.ts
@@ -4,7 +4,7 @@ import {useQuery} from '@tanstack/react-query';
 import {userService} from '../userService';
 
 export function useUserGetById(id: number) {
-  const {data, isLoading, isError} = useQuery({
+  const {data, isLoading, isError, isFetching, refetch} = useQuery({
     queryKey: [QueryKeys.UserGetById, id],
     queryFn: () => userService.getById(id),
     staleTime: 1000 * 30,
@@ -14,5 +14,7 @@ export function useUserGetById(id: number) {
     user: data,
     isError,
     isLoading,
+    isFetching,
+    refetch,
   };
 }
