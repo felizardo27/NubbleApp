37b524c2e6c5b631b32abf3cbee30d6b573b4358
felizardo27 <jp.felizardo27@gmail.com>
2024-05-24 14:31:48 -0300

    Authentication Flow: signUp domain and useCase

--- src/domain/Auth/authTypes.ts
+++ src/domain/Auth/authTypes.ts
@@ -18,3 +18,17 @@ export interface SignInData {
   email?: string;
   password: string;
 }
+
+export interface SignUpDataAPI {
+  firstName: string;
+  lastName: string;
+  username: string;
+  password: string;
+}
+
+export interface SignUpData {
+  firstName: string;
+  lastName: string;
+  username: string;
+  password: string;
+}
--- src/screens/auth/SignUpScreen/signUpSchema.ts
+++ src/screens/auth/SignUpScreen/signUpSchema.ts
@@ -1,3 +1,4 @@
+import {stringUtils} from '@utils';
 import {z} from 'zod';
 
 const userNameRegex = /^(?!.*\.\.)(?!.*\.$)[^\W][\w.]{0,29}$/gim;
@@ -7,16 +8,16 @@ export const signUpSchema = z.object({
     .string()
     .regex(userNameRegex, 'Nome de usuário inválido.')
     .toLowerCase(),
-  fullName: z
+  firstName: z
     .string()
     .min(5, 'Nome muito curto.')
     .max(50, 'Nome muito longo.')
-    .transform(value => {
-      return value
-        .split(' ')
-        .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
-        .join(' ');
-    }),
+    .transform(stringUtils.capitalizeFirstLetter),
+  lastName: z
+    .string()
+    .min(5, 'Nome muito curto.')
+    .max(50, 'Nome muito longo.')
+    .transform(stringUtils.capitalizeFirstLetter),
   email: z.string().email('E-mail inválido.'),
   password: z.string().min(8, 'A senha deve ter no mínimo 8 caracteres.'),
 });
