f6c5b2484cc01260c3d53a44695362c26d988391
João Pedro Felizardo <jp.felizardo27@gmail.com>
2024-06-03 23:03:06 -0300

    Tests with Jest: using report from coverage

--- src/utils/__test__/dateUtils.test.ts
+++ src/utils/__test__/dateUtils.test.ts
@@ -4,7 +4,7 @@ import {formatISO, sub} from 'date-fns';
 
 import {dateUtils} from '../dateUtils';
 
-const MOCKED_NOW = 1717380433;
+const MOCKED_NOW = 1749057505200;
 
 describe('dateUtils', () => {
   describe('formatRelative', () => {
@@ -19,14 +19,42 @@ describe('dateUtils', () => {
     test('should be displayed in seconds if less than 1 minute ago', () => {
       const time = sub(Date.now(), {seconds: 30});
       const timeISO = formatISO(time);
-
       expect(dateUtils.formatRelative(timeISO)).toBe('30 s');
     });
-    test('another test', () => {
-      const time = sub(Date.now(), {seconds: 30});
+    test('should be displayed in minutes if less than 1 hour ago', () => {
+      const time = sub(Date.now(), {minutes: 30});
       const timeISO = formatISO(time);
+      expect(dateUtils.formatRelative(timeISO)).toBe('30 m');
+    });
 
-      expect(dateUtils.formatRelative(timeISO)).toBe('30 s');
+    test('should be displayed in hours if less than 1 day ago', () => {
+      const time = sub(Date.now(), {hours: 15});
+      const timeISO = formatISO(time);
+      expect(dateUtils.formatRelative(timeISO)).toBe('15 h');
+    });
+
+    test('should be displayed in days if less than 7 days ago', () => {
+      const time = sub(Date.now(), {days: 5});
+      const timeISO = formatISO(time);
+      expect(dateUtils.formatRelative(timeISO)).toBe('5 d');
+    });
+
+    test('should be displayed in weeks if less than 4 weeks ago', () => {
+      const time = sub(Date.now(), {weeks: 3});
+      const timeISO = formatISO(time);
+      expect(dateUtils.formatRelative(timeISO)).toBe('3 sem');
+    });
+
+    test('should be displayed in months if less than 12 months ago', () => {
+      const time = sub(Date.now(), {months: 2});
+      const timeISO = formatISO(time);
+      expect(dateUtils.formatRelative(timeISO)).toBe('2 meses');
+    });
+
+    test('should be displayed in dd/MM/yyyy if more than 12 months ago', () => {
+      const time = sub(Date.now(), {months: 15});
+      const timeISO = formatISO(time);
+      expect(dateUtils.formatRelative(timeISO)).toBe('04/03/2024');
     });
   });
 });
