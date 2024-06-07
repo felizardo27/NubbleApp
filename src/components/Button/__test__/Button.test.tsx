928aeb40bfe3f8278d31a730f340670a6a9e5a76
João Pedro Felizardo <jp.felizardo27@gmail.com>
2024-06-06 23:23:47 -0300

    RN Testing Library: first test with render and debug

--- src/components/Button/__test__/Button.test.tsx
+++ src/components/Button/__test__/Button.test.tsx
@@ -1,11 +1,20 @@
 import React from 'react';
 
+import {ThemeProvider} from '@shopify/restyle';
 import {render} from '@testing-library/react-native';
+
+import {theme} from '@theme';
 
 import {Button} from '../Button';
 
 describe('<Button />', () => {
   test('The component rendered', () => {
-    render(<Button title="Button Title" />);
+    const {debug} = render(
+      <ThemeProvider theme={theme}>
+        <Button title="Button Title" />
+      </ThemeProvider>,
+    );
+
+    debug();
   });
 });
