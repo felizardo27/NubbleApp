20672d21d1030fe93a11dc5c1e29ab7c0e8f0838
felizardo27 <jp.felizardo27@gmail.com>
2024-06-27 13:27:29 -0300

    RN Hook Testing: setup react query

---
+++ src/domain/Auth/useCases/__tests__/useAuthSignIn.test.ts
@@ -0,0 +1,17 @@
+import {AllTheProviders, renderHook} from 'test-utils';
+
+import {useAuthSignIn} from '../useAuthSignIn';
+
+describe('useAuthSignIn', () => {
+  it('saves credentials if the sign-in successfully', () => {
+    const {result} = renderHook(() => useAuthSignIn(), {
+      wrapper: AllTheProviders,
+    });
+  });
+
+  it('calls the onError function with a message if sign-in fails', () => {
+    const {result} = renderHook(() => useAuthSignIn(), {
+      wrapper: AllTheProviders,
+    });
+  });
+});
