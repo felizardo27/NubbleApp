ef6d06225f324acf62b9e4103b852a7592ac3765
felizardo27 <jp.felizardo27@gmail.com>
2024-06-28 21:55:07 -0300

    RN Hook Testing: useAuthSignIn mocks

--- src/domain/Auth/useCases/__tests__/useAuthSignIn.test.ts
+++ src/domain/Auth/useCases/__tests__/useAuthSignIn.test.ts
@@ -1,17 +1,32 @@
-import {AllTheProviders, renderHook} from 'test-utils';
+import {AllTheProviders, renderHook, waitFor} from 'test-utils';
 
+import {authService} from '../../authService';
 import {useAuthSignIn} from '../useAuthSignIn';
 
+import {mockedAuthCredentials} from './mockedData/mocks';
+
 describe('useAuthSignIn', () => {
-  it('saves credentials if the sign-in successfully', () => {
+  it('saves credentials if the sign-in successfully', async () => {
+    jest
+      .spyOn(authService, 'signIn')
+      .mockResolvedValueOnce(mockedAuthCredentials);
+
     const {result} = renderHook(() => useAuthSignIn(), {
       wrapper: AllTheProviders,
     });
+
+    result.current.signIn({
+      email: 'mariajulia@coffstack.com',
+      password: 'supersecret',
+    });
+
+    await waitFor(() => expect(result.current.isSuccess).toBe(true));
   });
 
-  it('calls the onError function with a message if sign-in fails', () => {
-    const {result} = renderHook(() => useAuthSignIn(), {
-      wrapper: AllTheProviders,
-    });
-  });
+  // it('calls the onError function with a message if sign-in fails', () => {
+  //   // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  //   const {result} = renderHook(() => useAuthSignIn(), {
+  //     wrapper: AllTheProviders,
+  //   });
+  // });
 });
