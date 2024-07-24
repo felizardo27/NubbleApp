693ea09c43ac917e5d4d18bc04f6664f0430e657
João Pedro Felizardo <jp.felizardo27@gmail.com>
2024-07-23 22:53:40 -0300

    Integration tests: update interfaces

--- src/domain/PostComment/postCommentTypes.ts
+++ src/domain/PostComment/postCommentTypes.ts
@@ -1,3 +1,5 @@
+import {UserAPI} from '../User';
+
 export interface PostComment {
   id: number;
   message: string;
@@ -11,20 +13,12 @@ export interface PostComment {
   };
 }
 export interface PostCommentAPI {
-  id: number; // 115;
-  message: string; // 'Verecundia nulla vicinus.';
+  id: number; // 117;
+  message: string; // 'Pariatur cupiditate neque fugit pariatur ipsa.';
   user_id: number; // 6;
   post_id: number; // 1;
-  created_at: string; // '2024-04-21T05:26:52.000-03:00';
-  updated_at: string; // '2024-04-23T21:35:58.232-03:00';
-  user: {
-    id: number; // 6;
-    first_name: string; // 'Samuel';
-    last_name: string; // 'Vilar';
-    username: string; // samuelvilar';
-    email: string; // 'samu.vilar@coffstack.com';
-    profile_url: string; // 'https://nubble-development.s3.sa-east-1.amazonaws.com/backend-integration/5-samuel.png';
-    is_online: boolean; // false;
-    full_name: string; // 'Samuel Vilar';
-  };
+  created_at: string; // '2023-07-24T20:38:56.192+10:00';
+  updated_at: string; // '2023-07-24T20:38:56.192+10:00';
+  user: UserAPI;
+  meta: any; //{};
 }
