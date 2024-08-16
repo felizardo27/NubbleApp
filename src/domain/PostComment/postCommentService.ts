3d4c9068981a782f293ea48229b8a58b5e01c665
João Pedro Felizardo <jp.felizardo27@gmail.com>
2024-08-16 16:15:40 -0300

    User search: page adapter

--- src/domain/PostComment/postCommentService.ts
+++ src/domain/PostComment/postCommentService.ts
@@ -15,10 +15,10 @@ async function getList(
     per_page: PER_PAGE,
   });
 
-  return {
-    data: postCommentPageAPI.data.map(postCommentAdapter.toPostComment),
-    meta: apiAdapter.toMetaDataPage(postCommentPageAPI.meta),
-  };
+  return apiAdapter.toPageModel(
+    postCommentPageAPI,
+    postCommentAdapter.toPostComment,
+  );
 }
 
 async function create(post_id: number, message: string): Promise<PostComment> {
