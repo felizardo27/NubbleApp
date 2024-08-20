fdd5e6fcf64aa6e2fb161d69e3b9cfc41da6eab9
felizardo27 <jp.felizardo27@gmail.com>
2024-08-20 15:17:54 -0300

    User search: useUserSearch

--- src/screens/app/SearchScreen/SearchScreen.tsx
+++ src/screens/app/SearchScreen/SearchScreen.tsx
@@ -2,21 +2,29 @@ import React, {useState} from 'react';
 
 import {Icon, Screen, Text, TextInput} from '@components';
 import {AppScreenProps} from '@routes';
+import {useDebounce} from '@hooks';
+import {useUserSearch} from '@domain';
 
 export function SearchScreen({}: AppScreenProps<'SearchScreen'>) {
   const [search, setSearch] = useState('');
+  const searchDebounce = useDebounce(search);
+
+  const {list} = useUserSearch(searchDebounce);
 
   return (
     <Screen
       canGoBack
       HeaderComponent={
         <TextInput
+          placeholder="Digite sua busca"
           value={search}
           onChangeText={setSearch}
           leftComponent={<Icon name="search" color="gray3" size={16} />}
         />
       }>
-      <Text>Search Screen</Text>
+      {list.map(user => (
+        <Text key={user.id}>{user.username}</Text>
+      ))}
     </Screen>
   );
 }
