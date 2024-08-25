08a6be8347b0e597620dc0caf5d6667036dd9b90
João Pedro Felizardo <jp.felizardo27@gmail.com>
2024-08-25 13:12:14 -0300

    User search: fix bug in storage

--- App.tsx
+++ App.tsx
@@ -1,11 +1,7 @@
 import React from 'react';
 
 // import {ToastProvider} from '@services';
-import {
-  AuthCredentialsProvider,
-  initializeStorage,
-  MMKVStorage,
-} from '@services';
+
 import {ThemeProvider} from '@shopify/restyle';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
 import {SafeAreaProvider} from 'react-native-safe-area-context';
@@ -14,6 +10,9 @@ import {Toast} from '@components';
 import {theme} from '@theme';
 
 import {Router} from './src/routes/Routes';
+// import {AuthCredentialsProvider} from './src/services/authCredentials/Providers/AuthCredentialsProvider';
+import {AuthCredentialsProvider} from './src/services/authCredentials/Providers/authCredentialsProvider';
+import {initializeStorage, MMKVStorage} from './src/services/storage';
 
 initializeStorage(MMKVStorage);
 
