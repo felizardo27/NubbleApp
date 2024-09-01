a85328805b090a897f3d07b3d1904442f7d9daa8
João Pedro Felizardo <jp.felizardo27@gmail.com>
2024-09-01 11:13:15 -0300

    User search: mock API with MSW

--- src/domain/User/index.ts
+++ src/domain/User/index.ts
@@ -1,6 +1,6 @@
 export * from './useCases/useUserGetById';
 export * from './useCases/useUserSearch';
 export * from './userAdapter';
-export * from './userApi';
+export {USER_PATH} from './userApi';
 export * from './userService';
 export * from './userTypes';
--- src/test/server/server.ts
+++ src/test/server/server.ts
@@ -1,8 +1,10 @@
 import {setupServer} from 'msw/node';
 
 import {postCommentHandlers} from './postComment/postCommentHandlers';
+import {userHandler} from './user/userHandler';
 
-export const server = setupServer(...postCommentHandlers);
+export const server = setupServer(...postCommentHandlers, ...userHandler);
 
 export {mockedData as mockedPostComment} from './postComment/mocks';
+export {userMocked} from './user/userMocked';
 export {resetInMemoryResponse} from './postComment/postCommentHandlers';
