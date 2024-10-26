2da4992095421ca35efc36671818d6d5f64e8cd4
felizardo27 <jp.felizardo27@gmail.com>
2024-10-26 11:06:03 -0300

    onboardingFlow: onboarding images

--- src/screens/onBoarding/components/OnboardingPage.tsx
+++ src/screens/onBoarding/components/OnboardingPage.tsx
@@ -2,10 +2,14 @@ import React from 'react';
 
 import {Box} from '@components';
 
+import {ImageHeader} from './ImageHeader';
+
 export function OnboardingPage() {
   return (
     <Box flex={1} bg="background">
-      <Box flex={5} bg="error" />
+      <Box flex={5}>
+        <ImageHeader />
+      </Box>
       <Box flex={4} bg="carrotSecondary" />
       <Box flex={1} bg="success" />
     </Box>
