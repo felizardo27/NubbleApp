ab5a2265b3b54ce062e6796a1b14847e4f51ac63
felizardo27 <jp.felizardo27@gmail.com>
2024-10-27 12:29:42 -0300

    onboardingFlow:  onboarding page metadata

--- src/screens/onBoarding/components/BottomMenu.tsx
+++ src/screens/onBoarding/components/BottomMenu.tsx
@@ -4,10 +4,18 @@ import {Box, Icon, PressableBox, Text} from '@components';
 
 import {OnboardingPageProps} from './OnboardingPage';
 
+type BottomMenuProps = Pick<
+  OnboardingPageProps,
+  'onPressNext' | 'onPressSkip'
+> & {
+  isLast: boolean;
+};
 export function BottomMenu({
   onPressNext,
   onPressSkip,
-}: Pick<OnboardingPageProps, 'onPressNext' | 'onPressSkip'>) {
+  isLast,
+}: BottomMenuProps) {
+  const nexText = isLast ? 'Começar' : 'Próximo';
   return (
     <Box flexDirection="row" alignItems="center" justifyContent="space-between">
       <PressableBox onPress={onPressSkip} hitSlop={10}>
@@ -17,7 +25,7 @@ export function BottomMenu({
       </PressableBox>
       <PressableBox onPress={onPressNext} flexDirection="row">
         <Text mr="s4" bold>
-          Próximo
+          {nexText}
         </Text>
         <Icon name="arrowRight" color="carrotSecondary" />
       </PressableBox>
