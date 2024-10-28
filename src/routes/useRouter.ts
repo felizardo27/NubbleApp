bd1bddfa36f4c12182844ab23b39626f09a907ed
felizardo27 <jp.felizardo27@gmail.com>
2024-10-28 15:16:26 -0300

    Assets Icons and Splash: hide and test

--- src/routes/useRouter.ts
+++ src/routes/useRouter.ts
@@ -1,10 +1,22 @@
-import {useAuthCredentials, useShowOnboarding} from '@services';
+import {useEffect} from 'react';
+
+import {
+  settingsService,
+  useAuthCredentials,
+  useShowOnboarding,
+} from '@services';
 
 export type Stacks = 'Loading' | 'Auth' | 'App' | 'Onboarding';
 
 export function useRouter(): Stacks {
   const {authCredentials, isLoading} = useAuthCredentials();
   const showOnboarding = useShowOnboarding();
+
+  useEffect(() => {
+    if (!isLoading) {
+      settingsService.hideSplashScreen();
+    }
+  }, [isLoading]);
 
   if (isLoading) {
     return 'Loading';
