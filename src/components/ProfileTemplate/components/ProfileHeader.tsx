8a9bd55ca941106566479aff0d5e01686f8352db
felizardo27 <jp.felizardo27@gmail.com>
2024-12-19 22:48:38 -0300

    Profile Flow: button and columns

--- src/components/ProfileTemplate/components/ProfileHeader.tsx
+++ src/components/ProfileTemplate/components/ProfileHeader.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import {useNavigation} from '@react-navigation/native';
 
-import {Text, Box, ProfileAvatar, Icon} from '@components';
+import {Text, Box, ProfileAvatar, Icon, Button} from '@components';
 import {User} from '@domain';
 
 import {ProfileMetaData} from './ProfileMetaData';
@@ -16,28 +16,35 @@ export function ProfileHeader({user, isMyProfile}: Props) {
   const navigation = useNavigation();
 
   return (
-    <Box alignItems="center">
-      <ProfileAvatar imageURL={user?.profileUrl} size={100} borderRadius={40} />
-      <Text preset="headingMedium" mt="s16">
-        {user?.fullName}
-      </Text>
-      <Text preset="paragraphLarge" mt="s4" color="gray1">
-        @{user?.username}
-      </Text>
-      <ProfileMetaData
-        publicationCount="100"
-        followersCount="840"
-        followingCount="421"
-      />
-      {isMyProfile && (
-        <Box position="absolute" alignSelf="flex-end">
-          <Icon
-            size={30}
-            name="settings"
-            onPress={() => navigation.navigate('SettingsScreen')}
-          />
-        </Box>
-      )}
+    <Box paddingHorizontal="s24">
+      <Box alignItems="center">
+        <ProfileAvatar
+          imageURL={user?.profileUrl}
+          size={100}
+          borderRadius={40}
+        />
+        <Text preset="headingMedium" mt="s16">
+          {user?.fullName}
+        </Text>
+        <Text preset="paragraphLarge" mt="s4" color="gray1">
+          @{user?.username}
+        </Text>
+        <ProfileMetaData
+          publicationCount="100"
+          followersCount="840"
+          followingCount="421"
+        />
+        {isMyProfile && (
+          <Box position="absolute" alignSelf="flex-end">
+            <Icon
+              size={30}
+              name="settings"
+              onPress={() => navigation.navigate('SettingsScreen')}
+            />
+          </Box>
+        )}
+      </Box>
+      <Button title="Mensagem" marginVertical="s24" />
     </Box>
   );
 }
