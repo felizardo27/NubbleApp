e92b2e41afbede74e8c469003039bcd0848efa9a
felizardo27 <jp.felizardo27@gmail.com>
2025-01-12 17:34:12 -0300

    Profile Flow: useImperativeHandle

--- src/screens/app/EditProfileScreen/EditProfileScreen.tsx
+++ src/screens/app/EditProfileScreen/EditProfileScreen.tsx
@@ -1,10 +1,13 @@
-import React, {useState} from 'react';
+import React, {useRef, useState} from 'react';
 
 import {Button, Screen} from '@components';
 import {useUserGetById} from '@domain';
 import {AppScreenProps} from '@routes';
 
-import {EditProfileForm} from './components/EditProfileForm';
+import {
+  EditProfileForm,
+  EditProfileFormRef,
+} from './components/EditProfileForm';
 import {EditProfileHeader} from './components/EditProfileHeader';
 
 export function EditProfileScreen({
@@ -13,12 +16,22 @@ export function EditProfileScreen({
   const {user} = useUserGetById(route.params.userId);
   const [formIsValid, setFormIsValid] = useState(false);
 
-  function submitForm() {}
+  const formRef = useRef<EditProfileFormRef>(null);
+
+  function submitForm() {
+    formRef.current?.onSubmit();
+  }
 
   return (
     <Screen canGoBack title="Editar Perfil">
       <EditProfileHeader user={user} />
-      {user && <EditProfileForm user={user} onChangeIsValid={setFormIsValid} />}
+      {user && (
+        <EditProfileForm
+          ref={formRef}
+          user={user}
+          onChangeIsValid={setFormIsValid}
+        />
+      )}
 
       <Button
         title="Salvar alterações"
